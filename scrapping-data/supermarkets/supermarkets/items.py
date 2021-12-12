2a99a11659e713c8b1b330efbad3f745860c1e5e
nikodyulger <nikolasvdulgerov@gmail.com>
2021-12-12 07:55:41 +0100

    IN-34 #CarrefourPromoItem defined

--- scrapping-data/supermarkets/supermarkets/items.py
+++ scrapping-data/supermarkets/supermarkets/items.py
@@ -1,12 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
 class SupermarketsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name= scrapy.Field()
+    price = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
