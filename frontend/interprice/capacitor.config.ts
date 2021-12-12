cc47cbd8c8a0f11caa42dbcba990d888f345d99c
nikodyulger <nikolasvdulgerov@gmail.com>
2021-12-12 11:39:29 +0100

    IN-36 #Ionic Vue frontend app started

---
+++ frontend/interprice/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'interprice',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
