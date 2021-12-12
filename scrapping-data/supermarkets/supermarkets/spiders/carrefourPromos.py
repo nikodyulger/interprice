5f48a5ff3fa67e5e0aa2159501f40c9b611b4b69
nikodyulger <nikolasvdulgerov@gmail.com>
2021-12-12 10:39:22 +0100

    IN-34 #Modified spider to crawl several pages at once

--- scrapping-data/supermarkets/supermarkets/spiders/carrefourPromos.py
+++ scrapping-data/supermarkets/supermarkets/spiders/carrefourPromos.py
@@ -2,10 +2,12 @@ import scrapy
 from scrapy_playwright.page import PageCoroutine
 from ..items import CarrefourPromoItem
 
+
 class carrefourPromosSpider(scrapy.Spider):
     name = 'carrefourPromos'
     start_urls = [
-        'https://www.carrefour.es/supermercado/el-mercado-promocion/F-10flZ1023/c']
+        'https://www.carrefour.es/supermercado/el-mercado-promocion/F-10flZ1023/c',  # initial page
+        'https://www.carrefour.es/supermercado/el-mercado/cat20002/c?offset={offset}']  # rest of pages
     r_meta = dict(
         playwright=True,
         playwright_include_page=True,
@@ -13,22 +15,28 @@ class carrefourPromosSpider(scrapy.Spider):
             PageCoroutine(
                 "press", selector="body", key="PageDown"),
             PageCoroutine(
-                "wait_for_selector", "img[lazy='loaded']"),
+                "wait_for_selector", "img[lazy='loading']"),
+            # PageCoroutine("wait_for_timeout", 3000),
             PageCoroutine(
                 "press", selector="body", key="PageUp"),
             PageCoroutine("wait_for_timeout", 3000),
             PageCoroutine(
                 "press", selector="body", key="PageDown"),
-            # PageCoroutine("wait_for_timeout", 3000),
+            PageCoroutine("wait_for_timeout", 3000),
             # PageCoroutine(
             #     "screenshot", path="screenshot.png", full_page=True)
         ]
     )
+    num_pages = 3
 
     def start_requests(self):
         yield scrapy.Request(url=self.start_urls[0],
                              callback=self.parse,
                              meta=self.r_meta)
+        for offset in range(1,self.num_pages):
+            yield scrapy.Request(url=self.start_urls[-1].format(offset=offset*24),
+                                callback=self.parse,
+                                meta=self.r_meta)
 
     async def parse(self, response):
         promos = response.css(
@@ -46,6 +54,9 @@ class carrefourPromosSpider(scrapy.Spider):
                 price = price.strip()
 
             url_img = promo.css(
-                'img.product-card__image::attr(src)').get()
+                'img.product-card__image::attr(data-src)').get()
+            if url_img == None:
+                url_img = promo.css(
+                    'img.product-card__image::attr(src)').get()
 
             yield CarrefourPromoItem(name=name, price=price, image_urls=[url_img])
