3604fb26cca19c9b00b8114e49c7f8ede73118cb
nikodyulger <nikolasvdulgerov@gmail.com>
2022-05-06 17:27:54 +0200

    Cambios en función lambda data-pipeline

--- scrapping-data/lambda_functions/handler.py
+++ scrapping-data/lambda_functions/handler.py
@@ -13,23 +13,38 @@ def data_pipeline(event, context):
     password = os.environ['PASSWORD']
     db_name = os.environ['DB_NAME']
 
+    # Other paramaters
+    s3_image_url = os.environ['S3_IMAGE_URL']
+
     # Setting the log
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
+
+    # Initialize counters
+    new_items, changed_rows = 0,0
+
+    # Setting SNS client
+    sns_topic_arn = os.environ['SNS_TOPIC_ARN']
+    sns = boto3.client('sns')
 
     # S3 Object management to process data from
     bucket = 'supermarkets-interprice'
     key = f"data/{event['queryStringParameters']['key']}.json"
 
-    logger.info(f"INFO: Data Pipeline - {event['queryStringParameters']['key']}")
-    
+    logger.info(
+        f"INFO: Data Pipeline - {event['queryStringParameters']['key']}")
+
     s3 = boto3.client('s3')
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        data = json.loads(response['Body'].read()) 
+        data = json.loads(response['Body'].read())
     except Exception as e:
         logger.error("ERROR: Could not retrieve file from S3 Bucket")
         logger.error(e)
+        message = {
+            "statusCode": 200,
+            "body": "ERROR: Could not retrieve file from S3 Bucket"
+        }
         return "ERROR: Could not retrieve file from S3 Bucket"
 
     try:
@@ -41,10 +56,14 @@ def data_pipeline(event, context):
     except pymysql.MySQLError as e:
         logger.error("ERROR: Could not connect to AWS Aurora instance")
         logger.error(e)
+        message = {
+            "statusCode": 200,
+            "body": "ERROR: Could not connect to AWS Aurora instance"
+        }
         return "ERROR: Could not connect to AWS Aurora instance"
 
     logger.info("SUCCES: Connection to AWS RDS Aurora instance succeded")
-    
+
     with conn.cursor() as cur:
         for product in data:
 
@@ -53,10 +72,11 @@ def data_pipeline(event, context):
                     VALUES (%s, %s, %s, %s, %s, %s)"""
                 cur.execute(query,
                             (product['product_id'], product['supermarket'], product['name'],
-                             product['category'], product['image_urls'][0], product['images'][0]['path'])
+                             product['category'], product['image_urls'][0], f"{s3_image_url}{product['images'][0]['path']}")
                             )
                 logger.info(
                     f"Product ({product['product_id']},'{product['supermarket']}') inserted")
+                new_items += 1 
             except pymysql.err.MySQLError:
                 query = """INSERT INTO product (product_id, supermarket, name, category, image_url,image_url_s3) 
                     VALUES (%s, %s, %s, %s,%s,%s) 
@@ -68,11 +88,12 @@ def data_pipeline(event, context):
 
                 cur.execute(query,
                             (product['product_id'], product['supermarket'], product['name'], product['category'], product['image_urls'][0],
-                                product['images'][0]['path'], product['name'], product['category'], product['image_urls'][0], product['images'][0]['path'])
+                                product['images'][0]['path'], product['name'], product['category'], product['image_urls'][0], f"{s3_image_url}{product['images'][0]['path']}")
                             )
 
                 logger.info(
                     f"Product ({product['product_id']},'{product['supermarket']}') already exists, so it's updated")
+                changed_rows += 1
 
             conn.commit()
 
@@ -121,29 +142,20 @@ def data_pipeline(event, context):
     conn.commit()
     conn.close()
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps('Everything went OK!!')
-    }
-
-
-def notify(event, context):
-
-    sns_topic_arn = os.environ['SNS_TOPIC_ARN']
-
-    sns_client = boto3.client('sns')
-
     message = {
         "statusCode": 200,
-        "body": json.dumps(event)
+        "body": json.dumps({
+            "msg": "Everything when OK!!",
+            "new_items": new_items,
+            "changed_rows": changed_rows
+        })
     }
-
-    response = sns_client.publish(
+    response = sns.publish(
         TopicArn=sns_topic_arn,
         Message=json.dumps(message)
     )
 
     return {
-        'statusCode': 200,
-        'body': json.dumps(response)
+        "statusCode": 200,
+        "body": json.dumps('Everything went OK!!')
     }
