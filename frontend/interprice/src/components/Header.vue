7e00ce4351dbd7b2c167b00413b7249163fe6d9a
nikodyulger <nikolasvdulgerov@gmail.com>
2022-05-22 23:17:43 +0200

    Componente Header

---
+++ frontend/interprice/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-header class="ion-no-border">
+    <ion-toolbar color="tertiary">
+      <ion-title slot="start">
+        <ion-text @click="() => router.push('/')">Interprice</ion-text>
+      </ion-title>
+    </ion-toolbar>
+  </ion-header>
+</template>
+
+<script lang="ts">
+import { IonHeader, IonToolbar, IonText } from "@ionic/vue";
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonText,
+  },
+  data() {
+    return {
+      search: "",
+    };
+  },
+  setup() {
+    const router = useRouter();
+    return { router };
+  },
+});
+</script>
+
+<style scoped>
+</style>
