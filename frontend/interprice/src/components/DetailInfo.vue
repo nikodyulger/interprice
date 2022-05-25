35b5581fc3dc600d47a23bd246352023e6d1d164
Nikola <nikolasvdulgerov@gmail.com>
2022-05-25 23:54:36 +0200

    Componente detalles producto

---
+++ frontend/interprice/src/components/DetailInfo.vue
@@ -0,0 +1,91 @@
+<template>
+    <ion-card v-if="!isEmpty">
+        <ion-card-header>
+            <ion-card-title>
+                <ion-text>Más información</ion-text>
+            </ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+            <ion-list>
+                <ion-item-group v-if=ingredients>
+                    <ion-item-divider mode="ios">
+                        <ion-label>Ingredientes</ion-label>
+                    </ion-item-divider>
+                    <ion-item lines="none">
+                        {{ ingredients }}
+                    </ion-item>
+                </ion-item-group>
+
+                <ion-item-group v-if=preparation>
+                    <ion-item-divider mode="ios">
+                        <ion-label>Preparación</ion-label>
+                    </ion-item-divider>
+                    <ion-item lines="none">
+                        {{ preparation }}
+                    </ion-item>
+                </ion-item-group>
+
+                <ion-item-group v-if=storage>
+                    <ion-item-divider mode="ios">
+                        <ion-label>Conservación</ion-label>
+                    </ion-item-divider>
+                    <ion-item lines="none">
+                        {{ storage }}
+                    </ion-item>
+                </ion-item-group>
+
+                <ion-item-group>
+                    <ion-item-divider mode="ios" v-if=manufacturer>
+                        <ion-label>Fabricante</ion-label>
+                    </ion-item-divider>
+                    <ion-item lines="none">
+                        {{ manufacturer }}
+                    </ion-item>
+                </ion-item-group>
+            </ion-list>
+        </ion-card-content>
+    </ion-card>
+</template>
+
+<script lang="ts">
+import {
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonItem,
+    IonItemGroup,
+    IonLabel,
+    IonText,
+    IonList,
+} from "@ionic/vue";
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "Breadcrumbs",
+    props: {
+        ingredients: String,
+        preparation: String,
+        storage: String,
+        manufacturer: String
+    },
+    components: {
+        IonCard,
+        IonCardContent,
+        IonCardHeader,
+        IonCardSubtitle,
+        IonCardTitle,
+        IonItem,
+        IonItemGroup,
+        IonLabel,
+        IonText,
+        IonList,
+    },
+    computed: {
+        isEmpty(): boolean {
+            return Object.values(this.$props).every(p => p === null || p === '');
+        }
+    }
+})
+</script>
