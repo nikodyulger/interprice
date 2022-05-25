55025b3a7ae0ce4668dbdb604136ef7258ef6cb1
Nikola <nikolasvdulgerov@gmail.com>
2022-05-25 23:53:57 +0200

    Componente Breadcrumbs

---
+++ frontend/interprice/src/components/Breadcrumbs.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ion-chip outline>Frescos</ion-chip>
+        <ion-chip outline> {{ category }} </ion-chip>
+        <ion-chip outline v-if="subcategory"> {{ subcategory }} </ion-chip>
+    </div>
+</template>
+
+<script lang="ts">
+import {
+    IonChip
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: "Breadcrumbs",
+    props: ['category', 'subcategory'],
+    components: {
+        IonChip
+    }
+})
+</script>
