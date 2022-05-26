30949de2b24b452297dee8875121cc8e52762801
Nikola <nikolasvdulgerov@gmail.com>
2022-05-26 23:00:58 +0200

    Vista Principal añadidos modal y fab

--- frontend/interprice/src/views/Home.vue
+++ frontend/interprice/src/views/Home.vue
@@ -1,121 +1,97 @@
 <template>
-  <ion-page>
-    <Header />
-    <br />
-    <ion-content fullscreen class="ion-padding">
-      <ion-grid>
-        <ion-row>
-          <ion-searchbar
-            color="light"
-            enterkeyhint="enter"
-            inputmode="search"
-            type="search"
-            @search="getSearchedInput($event)"
-            v-model="search"
-            placeholder="Busca productos"
-            search-icon="search-outline"
-            animated
-          ></ion-searchbar>
-        </ion-row>
-        <ion-row>
-          <ion-col>
-            <Filters />
-          </ion-col>
-        </ion-row>
-        <ion-row class="ion-text-end">
-          <ion-col pull="1">
-            <ion-text>Ordenar por: </ion-text>
-
-            <ion-text>A-Z</ion-text>
-            |
-            <ion-text>Precio</ion-text>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <!-- <ion-col size="3">
-          </ion-col> -->
-
-          <ion-col>
-            <ProductCard />
-          </ion-col>
-          <ion-col>
-            <ProductCard />
-          </ion-col>
-          <ion-col>
-            <ProductCard />
-          </ion-col>
-          <ion-col>
-            <ProductCard />
-          </ion-col>
-          <ion-col>
-            <ProductCard />
-          </ion-col>
-          <ion-col>
-            <ProductCard />
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <PagesControl/>
-        </ion-row>
-      </ion-grid>
-    </ion-content>
-  </ion-page>
+  <keep-alive>
+    <ion-page>
+      <Header />
+      <br />
+      <ion-content fullscreen class="ion-padding ">
+        <Filters />
+        <ion-grid class="catalog">
+          <ion-row v-for="row in catalog.catalog" :key="row">
+            <ion-col v-for="prod in row" :key="prod.product_id" size-md="2">
+              <ProductCard :name="prod.name" :productId="prod.product_id" :supermarket="prod.supermarket"
+                :imageUrl="prod.image_url_s3" :price="prod.prices[0].price" />
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+        <Fab />
+        <ion-modal :is-open="list.show" @didDismiss="list.hideList">
+          <Modal/>
+        </ion-modal>
+        <br />
+        <br />
+        <PagesControl />
+      </ion-content>
+    </ion-page>
+  </keep-alive>
 </template>
 
 <script lang="ts">
-import axios from "axios";
 import {
   IonPage,
-  IonText,
   IonContent,
   IonGrid,
   IonRow,
   IonCol,
-  IonSearchbar,
+  IonModal
 } from "@ionic/vue";
 
-// import PromoListItem from "@/components/PromoListItem.vue";
-import Header from "@/components/Header.vue";
-import ProductCard from "@/components/ProductCard.vue";
-import Filters from "@/components/Filters.vue";
-import PagesControl from "@/components/PagesControl.vue";
+import Header from "../components/Header.vue";
+import ProductCard from "../components/ProductCard.vue";
+import Filters from "../components/Filters.vue";
+import PagesControl from "../components/PagesControl.vue";
+import Fab from "../components/Fab.vue";
+import Modal from "../components/Modal.vue";
 import { defineComponent } from "vue";
+import { useCatalogStore } from "@/store/catalog";
+import { useListStore } from "@/store/list";
 
 export default defineComponent({
   name: "Home",
-  data() {
-    return {
-      promos: null,
-      search: "",
-    };
-  },
-  mounted() {
-    axios
-      .get("http://127.0.0.1:8000/promos")
-      .then((res) => (this.promos = res.data))
-      .catch((err) => console.log(err.message));
-  },
   components: {
     IonPage,
-    IonText,
     IonContent,
     IonGrid,
     IonRow,
     IonCol,
-    IonSearchbar,
+    IonModal,
     Header,
     Filters,
+    Fab,
+    Modal,
     ProductCard,
     PagesControl
   },
   methods: {
-    getSearchedInput(event: Event) {
-      console.log(event);
-      console.log(this.search);
-    },
+
   },
+  data() {
+    return {
+      search: "",
+    };
+  },
+  beforeMount() {
+    this.catalog.getProducts();
+  },
+  setup() {
+    const catalog = useCatalogStore()
+    const list = useListStore();
+    // const catalog = computed(() => {
+    //   const arr: Product[][] = [];
+    //   for (let i = 0; i < store.products.length; i += 5) {
+    //     arr.push(store.products.slice(i, i + 5))
+    //   }
+    //   return arr;
+    // })
+
+    return {
+      catalog,
+      list
+    }
+  }
 });
 </script>
 <style scoped>
-
+ion-card {
+  padding: 5px;
+}
 </style>
