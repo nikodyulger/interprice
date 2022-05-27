8c29160eb81ae0279c8b28e7a2c60fbc05b6ea29
Nikola <nikolasvdulgerov@gmail.com>
2022-05-27 20:47:00 +0200

    Filtro en catalogo store

--- frontend/interprice/src/store/catalog.ts
+++ frontend/interprice/src/store/catalog.ts
@@ -17,36 +17,48 @@ export const useCatalogStore = defineStore({
       return (productId: number, supermarket: string) => {
         return state.products.find((p) => p.product_id === productId && p.supermarket === supermarket)
       }
-    }
-  //   filterByCategory: (state) => {
-  //     return (categories: string[]) => {
-  //       state.catalog = [[]];
-  //       state.filteredProducts = state.products.filter((p) =>
-  //         categories.includes(p.category)
-  //       );
-  //       console.log(state.filteredProducts);
-  //       for (let i = 0; i < state.filteredProducts.length; i += 6) {
-  //         state.catalog.push(state.filteredProducts.slice(i, i + 6));
-  //       }
-  //     };
-  //   },
+    },
+    filterByCategory: (state) => {
+      return (filters: any) => {
+        state.catalog = [[]];
+        const prod = state.products.filter((p) => {
+          let res = true
+
+          if (filters.category.length > 0)
+            res = res && filters.category.includes(p.category) 
+            
+          if(filters.supermarket.length > 0)
+            res = res && filters.supermarket.includes(p.supermarket)
+            
+
+          // if(filters.price.length > 0)
+          //   filters.price.includes(p.prices)
+          return res
+        }
+
+        );
+        for (let i = 0; i < prod.length; i += 6) {
+          state.catalog.push(prod.slice(i, i + 6));
+        }
+      };
+    },
   },
   actions: {
     orderByPrice() {
       this.products.sort((a, b) =>
         a.prices[0].price < b.prices[0].price ? -1 : 1
       );
+      this.currentPage = 1;
       this.getCatalog();
     },
     orderByName() {
       this.products.sort((a, b) => (a.name < b.name ? -1 : 1));
+      this.currentPage = 1;
       this.getCatalog();
     },
     getCatalog() {
       this.catalog = [[]];
-      console.log((this.currentPage - 1)*30,(this.currentPage - 1)*30 + 30 )
       const prods = this.products.slice((this.currentPage - 1)*30, (this.currentPage - 1)*30 + 30)
-      console.log(prods)
       for (let i = 0; i < prods.length ; i += 6) {
         this.catalog.push(prods.slice(i, i + 6));
       }
