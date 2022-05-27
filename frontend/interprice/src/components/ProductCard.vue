ee396a637d039bcde6996510bd56e7ee05980d37
Nikola <nikolasvdulgerov@gmail.com>
2022-05-27 20:44:05 +0200

    Botones tarjeta producto

--- frontend/interprice/src/components/ProductCard.vue
+++ frontend/interprice/src/components/ProductCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <ion-card class="ion-text-center ion-align-items-stretch">
+  <ion-card class="ion-text-center">
     <img
       @click="() => router.push({ name: 'ProductDetails', params: { supermarket: supermarket, productId: productId } })"
       :src=imageUrl height="150" width="150" />
@@ -17,9 +17,18 @@
     <ion-card-content class="ion-text-start">
       <ion-text><strong>{{ $filters.currency(price) }}</strong></ion-text>
     </ion-card-content>
-    <ion-item lines="none">
-      <ion-button size="small" slot="end" @click="addProduct">Comparar</ion-button>
-    </ion-item>
+    <ion-grid>
+      <ion-row class="ion-justify-content-around">
+        <ion-col size-sm="4" size="4" class="ion-text-center">
+          <ion-button size="small" expand="block" shape="round" color="success" @click="addProductToCart">
+            <ion-icon slot="icon-only" :icon="bagAdd"></ion-icon>
+          </ion-button>
+        </ion-col>
+        <ion-col size-sm="6" size="6" class="ion-text-center">
+          <ion-button size="small" expand="block" @click="addProductToList">Comparar</ion-button>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
   </ion-card>
 </template>
 
@@ -27,13 +36,23 @@
 import { defineComponent } from "vue";
 import { useListStore } from "@/store/list";
 import { useCatalogStore } from "@/store/catalog";
+import { useCartStore } from "@/store/cart";
 import {
   IonItem,
   IonCard,
   IonCardHeader,
   IonCardContent,
+  IonCardTitle,
   IonCardSubtitle,
+  IonText,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonGrid,
+  IonCol,
+  IonRow
 } from "@ionic/vue";
+import { bagAdd } from "ionicons/icons";
 import { useRouter } from "vue-router";
 
 export default defineComponent({
@@ -43,7 +62,15 @@ export default defineComponent({
     IonCard,
     IonCardHeader,
     IonCardContent,
+    IonCardTitle,
     IonCardSubtitle,
+    IonText,
+    IonButton,
+    IonButtons,
+    IonIcon,
+    IonGrid,
+    IonCol,
+    IonRow
   },
   props: [
     "productId", "supermarket", "name", "imageUrl", "price",
@@ -52,14 +79,21 @@ export default defineComponent({
     const router = useRouter();
     const list = useListStore();
     const catalog = useCatalogStore();
+    const cart = useCartStore();
     return {
       router,
       list,
-      catalog
+      catalog,
+      cart,
+      bagAdd
     };
   },
   methods: {
-    addProduct() {
+    addProductToCart() {
+      const prod = this.catalog.getProduct(this.$props.productId, this.$props.supermarket);
+      this.cart.addProduct(prod);
+    },
+    addProductToList() {
       const prod = this.catalog.getProduct(this.$props.productId, this.$props.supermarket);
       this.list.addProduct(prod);
     }
