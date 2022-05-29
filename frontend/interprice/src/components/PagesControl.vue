f16c6ac4e9e41032cc3a102d284a9112b0b14c8a
Nikola <nikolasvdulgerov@gmail.com>
2022-05-29 22:41:41 +0200

    Arreglo de cambios de página

--- frontend/interprice/src/components/PagesControl.vue
+++ frontend/interprice/src/components/PagesControl.vue
@@ -1,17 +1,17 @@
 <template>
   <ion-toolbar>
-    <ion-button shape="round" slot="start" @click="previousPage">
+    <ion-button shape="round" slot="start" @click="catalog.previousPage" :disabled="catalog.currentPage === 1">
       <ion-icon slot="icon-only" :icon="arrowBackOutline" />
     </ion-button>
     <ion-title>
       <ion-chip>
         <ion-label>Página</ion-label>
-        <ion-select :value="store.currentPage" interface="action-sheet" @ionChange="changePage">
-          <ion-select-option v-for="p in store.pages" :value="p" :key="p">{{ p }}</ion-select-option>
+        <ion-select :value="catalog.currentPage" :selectedText="catalog.currentPage" interface="action-sheet" @ionChange="catalog.changePage(Number($event.detail.value))">
+          <ion-select-option v-for="p in catalog.pages" :key="p" :value="p" >{{p}}</ion-select-option>
         </ion-select>
       </ion-chip>
     </ion-title>
-    <ion-button shape="round" slot="end" @click="nextPage">
+    <ion-button shape="round" slot="end" @click="catalog.nextPage" :disabled="catalog.currentPage === catalog.pages">
       <ion-icon slot="icon-only" :icon="arrowForwardOutline" />
     </ion-button>
   </ion-toolbar>
@@ -34,9 +34,9 @@ import {
 export default defineComponent({
   name: "PagesControl",
   setup() {
-    const store = useCatalogStore()
+    const catalog = useCatalogStore()
     return {
-      store,
+      catalog,
       arrowBackOutline,
       arrowForwardOutline,
     };
@@ -50,18 +50,7 @@ export default defineComponent({
     IonSelect,
     IonSelectOption,
     IonTitle
-  },
-  methods: {
-    previousPage() {
-      this.store.previousPage();
-    },
-    nextPage() {
-      this.store.nextPage();
-    },
-    changePage(event: any) {
-      this.store.changePage(event.detail.value);
-    }
-  },
+  }
 });
 </script>
 <style scoped>
