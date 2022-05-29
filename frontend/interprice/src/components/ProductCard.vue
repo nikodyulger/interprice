9fcadbf7ce2190bb82a6f8bbab3afa6999714677
Nikola <nikolasvdulgerov@gmail.com>
2022-05-29 22:42:11 +0200

    Cambio de props y funcionalidad toast

--- frontend/interprice/src/components/ProductCard.vue
+++ frontend/interprice/src/components/ProductCard.vue
@@ -1,31 +1,33 @@
 <template>
   <ion-card class="ion-text-center">
     <img
-      @click="() => router.push({ name: 'ProductDetails', params: { supermarket: supermarket, productId: productId } })"
-      :src=imageUrl height="150" width="150" />
+      @click="() => router.push({ name: 'ProductDetails', params: { supermarket: product.supermarket, productId: product.product_id } })"
+      :src=product.image_url height="150" width="150" />
     <ion-card-header class="ion-text-start" button
       router-link="{ name: 'ProductDetails', params: { supermarket: supermarket, productId: productId}}">
       <ion-card-subtitle>
-        <ion-text>{{ supermarket }}</ion-text>
+        <ion-text>{{ product.supermarket }}</ion-text>
       </ion-card-subtitle>
       <ion-card-title>
         <ion-text>
-          <h5>{{ name }}</h5>
+          <h5>{{ product.name }}</h5>
         </ion-text>
       </ion-card-title>
     </ion-card-header>
     <ion-card-content class="ion-text-start">
-      <ion-text><strong>{{ $filters.currency(price) }}</strong></ion-text>
+      <ion-text><strong>{{ $filters.currency(product.prices[0].price) }}</strong></ion-text>
     </ion-card-content>
     <ion-grid>
       <ion-row class="ion-justify-content-around">
-        <ion-col size-sm="4" size="4" class="ion-text-center">
-          <ion-button size="small" expand="block" shape="round" color="success" @click="addProductToCart">
+        <ion-col size-sm="4" size="4">
+          <ion-button size="small" expand="block" shape="round" color="success"
+            @click="cart.addProduct(product) && toastProduct('Producto añadido a la cesta')">
             <ion-icon slot="icon-only" :icon="bagAdd"></ion-icon>
           </ion-button>
         </ion-col>
         <ion-col size-sm="6" size="6" class="ion-text-center">
-          <ion-button size="small" expand="block" @click="addProductToList">Comparar</ion-button>
+          <ion-button size="small" expand="block" @click="list.addProduct(product) && toastProduct('Producto añadido para comparar')">Comparar
+          </ion-button>
         </ion-col>
       </ion-row>
     </ion-grid>
@@ -35,7 +37,6 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useListStore } from "@/store/list";
-import { useCatalogStore } from "@/store/catalog";
 import { useCartStore } from "@/store/cart";
 import {
   IonItem,
@@ -50,7 +51,8 @@ import {
   IonIcon,
   IonGrid,
   IonCol,
-  IonRow
+  IonRow,
+  toastController
 } from "@ionic/vue";
 import { bagAdd } from "ionicons/icons";
 import { useRouter } from "vue-router";
@@ -73,30 +75,30 @@ export default defineComponent({
     IonRow
   },
   props: [
-    "productId", "supermarket", "name", "imageUrl", "price",
+    "product"
   ],
   setup() {
     const router = useRouter();
     const list = useListStore();
-    const catalog = useCatalogStore();
     const cart = useCartStore();
     return {
       router,
       list,
-      catalog,
       cart,
       bagAdd
     };
   },
   methods: {
-    addProductToCart() {
-      const prod = this.catalog.getProduct(this.$props.productId, this.$props.supermarket);
-      this.cart.addProduct(prod);
+    async toastProduct(message: string) {
+      const toast = await toastController
+        .create({
+          message: message,
+          duration: 500,
+          color: 'tertiary',
+          position: 'bottom'
+        })
+      return toast.present();
     },
-    addProductToList() {
-      const prod = this.catalog.getProduct(this.$props.productId, this.$props.supermarket);
-      this.list.addProduct(prod);
-    }
   }
 });
 </script>
