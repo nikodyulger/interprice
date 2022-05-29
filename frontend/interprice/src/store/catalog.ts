b42579352e459db6430b4a6216882c114ca11e5e
Nikola <nikolasvdulgerov@gmail.com>
2022-05-29 22:44:25 +0200

    Arreglo bugs y cambio logica en catalogo

--- frontend/interprice/src/store/catalog.ts
+++ frontend/interprice/src/store/catalog.ts
@@ -5,41 +5,49 @@ import API from "../services/apiService";
 export const useCatalogStore = defineStore({
   id: "catalog",
   state: () => ({
-    currentPage: 1,
     products: [] as Product[],
+    filteredProducts: [] as Product[],
     catalog: [[]] as Product[][],
+    currentPage: 1
   }),
   getters: {
-    pages(): number{
-      return Math.ceil(this.products.length/30);
+    pages(): number {
+      return Math.ceil(this.products.length / 30);
     },
     getProduct: (state) => {
       return (productId: number, supermarket: string) => {
-        return state.products.find((p) => p.product_id === productId && p.supermarket === supermarket)
-      }
+        return state.products.find(
+          (p) => p.product_id === productId && p.supermarket === supermarket
+        );
+      };
     },
-    filterByCategory: (state) => {
+    filterProducts: (state) => {
       return (filters: any) => {
         state.catalog = [[]];
+
         const prod = state.products.filter((p) => {
-          let res = true
+          let res = true;
 
-          if (filters.category.length > 0)
-            res = res && filters.category.includes(p.category) 
-            
-          if(filters.supermarket.length > 0)
-            res = res && filters.supermarket.includes(p.supermarket)
-            
+          if (filters.categories.length > 0)
+            res = res && filters.categories.includes(p.category);
 
-          // if(filters.price.length > 0)
-          //   filters.price.includes(p.prices)
-          return res
-        }
+          if (filters.supermarkets.length > 0)
+            res = res && filters.supermarkets.includes(p.supermarket);
 
-        );
+          res =
+            res &&
+            filters.price.lower <= p.prices[0].price &&
+            p.prices[0].price <= filters.price.upper;
+
+          return res;
+        });
+
         for (let i = 0; i < prod.length; i += 6) {
           state.catalog.push(prod.slice(i, i + 6));
         }
+
+        state.filteredProducts = prod;
+        state.currentPage = 1;
       };
     },
   },
@@ -58,40 +66,43 @@ export const useCatalogStore = defineStore({
     },
     getCatalog() {
       this.catalog = [[]];
-      const prods = this.products.slice((this.currentPage - 1)*30, (this.currentPage - 1)*30 + 30)
-      for (let i = 0; i < prods.length ; i += 6) {
-        this.catalog.push(prods.slice(i, i + 6));
+      let prods = [];
+      console.log(this.filteredProducts.length)
+      this.filteredProducts.length > 0
+        ? (prods = this.filteredProducts.slice(
+            (this.currentPage - 1) * 30,
+            (this.currentPage - 1) * 30 + 30
+          ))
+        : (prods = this.products.slice(
+            (this.currentPage - 1) * 30,
+            (this.currentPage - 1) * 30 + 30
+          ));
+
+      for (let i = 0; i < prods.length; i += 5) {
+        this.catalog.push(prods.slice(i, i + 5));
       }
     },
-    nextPage(){
-      this.currentPage += 1;
+    nextPage() {
+      if(this.currentPage !== this.pages) (this.currentPage += 1) ;
       this.getCatalog();
     },
-    previousPage(){
-      if(this.currentPage !== 1){
-        this.currentPage -= 1;
-      }
-      this.getCatalog();
+    previousPage() {
+      if(this.currentPage !== 1) (this.currentPage -= 1);
+      this.getCatalog(); 
     },
-    changePage(page: number){
+    changePage(page: number) {
       this.currentPage = page;
       this.getCatalog();
     },
-    getProducts() {
-      API.getProducts()
+    getSearchedProducts(q: string) {
+      API.getSearchedProducts(q)
         .then((res) => {
           this.products = res.data;
+          this.filteredProducts = [];
+          this.currentPage = 1;
           this.getCatalog();
         })
         .catch((err) => console.log(err.message));
     },
-    getSearchedProducts(q: string){
-      API.getSearchedProducts(q)
-      .then((res) => {
-        this.products = res.data;
-        this.getCatalog();
-      })
-      .catch((err) => console.log(err.message));
-    }
   },
 });
