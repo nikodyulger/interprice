b67e642a0425dc5a4daf6135183514e87a998bbf
Nikola <nikolasvdulgerov@gmail.com>
2022-05-31 11:13:19 +0200

    Pagina Detalles de producto ajustes en interfaz

--- frontend/interprice/src/views/ProductDetails.vue
+++ frontend/interprice/src/views/ProductDetails.vue
@@ -2,34 +2,29 @@
   <ion-page>
     <Header />
     <ion-content fullscreen="true" class="ion-padding">
-      <ion-loading :is-open="!loading" message="Por favor, espere..."></ion-loading>
+      <ion-loading :is-open="loading" message="Por favor, espere..."></ion-loading>
       <ion-grid>
         <ion-row>
-          <Breadcrumbs v-if="loading" :category="product.category"
+          <Breadcrumbs v-if="!loading" :category="product.category"
             :subcategory="product.details ? product.details.subcategory : ''" />
         </ion-row>
       </ion-grid>
       <ion-grid>
         <ion-row>
-          <ion-col>
-            <ProductInfo v-if="loading" v-bind="product" />
+          <ion-col size-sm="12" size-md="6">
+            <ProductInfo v-if="!loading" v-bind="product" />
           </ion-col>
-          <ion-col>
-            <ion-card style="height: 100%" class="ion-padding">
-              <ion-card-header>
-                <ion-card-title>
-                  <ion-text>Histórico de precios</ion-text>
-                </ion-card-title>
-              </ion-card-header>
-              <ion-card-content>
-                <PriceChart />
-              </ion-card-content>
-            </ion-card>
+          <ion-col size-sm="12" size-md="6">
+            <PriceChart v-if="!loading" :prices="product.prices" />
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col soze="10">
+            <DetailInfo v-if="product.details" v-bind="product.details" />
           </ion-col>
         </ion-row>
       </ion-grid>
       <br />
-      <DetailInfo v-if="product.details" v-bind="product.details" />
       <ion-modal :is-open="list.show" @didDismiss="list.hideList">
         <Modal />
       </ion-modal>
@@ -42,6 +37,9 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useListStore } from "@/store/list";
+import { Product } from "../types/main";
+import API from "../services/apiService";
+
 import {
   IonPage,
   IonContent,
@@ -57,6 +55,7 @@ import {
   IonLoading,
   IonModal
 } from "@ionic/vue";
+
 import Header from "../components/Header.vue";
 import PriceChart from "../components/PriceChart.vue";
 import ProductInfo from "../components/ProductInfo.vue";
@@ -65,8 +64,6 @@ import DetailInfo from "../components/DetailInfo.vue";
 import Modal from "../components/Modal.vue";
 import Fab from "../components/Fab.vue";
 import Footer from "../components/Footer.vue";
-import { Product } from "../types/main";
-import API from "../services/apiService";
 
 export default defineComponent({
   name: "ProductDetails",
@@ -96,11 +93,11 @@ export default defineComponent({
   },
   data() {
     return {
-      loading: false,
+      loading: true,
       product: {} as Product
     }
   },
-  setup(){
+  setup() {
     const list = useListStore();
     return {
       list
@@ -110,7 +107,7 @@ export default defineComponent({
     API.getProductDetails(this.supermarket, this.productId)
       .then((res) => {
         this.product = res.data;
-        this.loading = true;
+        this.loading = false;
       })
       .catch((err) => console.log(err.message));
   }
