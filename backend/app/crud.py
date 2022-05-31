592b5af6ab2de6e8bb84b1f0109592f93900d49f
nikodyulger <nikolasvdulgerov@gmail.com>
2022-05-31 11:17:14 +0200

    Cambios en queries crud productos

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,25 +1,20 @@
+from jmespath import search
 from sqlalchemy.orm import Session
-from sqlalchemy import and_
-from typing import List
+from sqlalchemy.sql.expression import func 
 from .models import Product
 
 
 def get_product(db: Session, p_id: int, supermarket: str):
     return db.query(Product).filter(Product.product_id == p_id and Product.supermarket == supermarket).first()
 
-def get_products(db: Session, skip: int = 0, limit: int = 100, q_name: str = None):
-    if q_name:
-        return db.query(Product).filter(Product.name.contains(q_name)).offset(skip).limit(limit).all()
-    else:
-        return db.query(Product).offset(skip).limit(limit).all()
+
+def get_products(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(Product).order_by(func.rand()).offset(skip).limit(limit).all()
+
 
 def get_categories(db: Session):
-    return db.query(Product.supermarket,Product.category).distinct(Product.category).all()
+    return db.query(Product.supermarket, Product.category).distinct(Product.category).all()
 
-def get_filtered_products(db: Session, cat: List[str] = None, sup: List[str] = None):
-    if cat == None and sup != None:
-        return  db.query(Product).filter(Product.supermarket.in_(sup)).all()
-    elif cat != None and sup == None:
-        return  db.query(Product).filter(Product.category.in_(cat)).all()
-    else:
-        return db.query(Product).filter(and_(Product.category.in_(cat), Product.supermarket.in_(sup))).all()
+
+def get_filtered_products(db: Session, q: str):
+    return db.query(Product).filter(Product.name.contains(q)).all()
